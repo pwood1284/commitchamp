63055c496bd2918c210ce54854ac9b5d39c0cea3
pwood1284@gmail.com <your_email@whatever.com>
2015-05-31 12:32:32 -0400

    github.rb commit

--- lib/commitchamp/github.rb
+++ lib/commitchamp/github.rb
@@ -1,8 +1,29 @@
 require 'httparty'
 
 module Commitchamp
+  ACCESS_TOKEN = ENV['ACCESS_TOKEN']
+
   class Github
     include HTTParty
     base_uri "https://api.github.com"
+
+    def initialize
+      @headers = { "Authorization" => "token #{ACCESS_TOKEN}",
+                   "User-Agent" => "HTTParty" }
+    end
+
+    def get_user(username)
+      self.class.get("/users/#{username}", headers: @headers)
+    end
+
+    # def get_contributions(owner, repo, page=1)
+    #   params = {
+    #     page: page
+    #   }
+    #   options = {
+    #     headers: @headers,
+    #     query: params
+    #   }
+    #   self.class.get("/repos/#{owner}/#{repo}/stats/contributors", options)
   end
 end
